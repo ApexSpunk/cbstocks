ea51eee3f800ca1052c75b1f054f3bfc84c490bb
Chandan <indspunk@gmail.com>
2023-01-30 23:36:59 +0530

    Done

--- backend/src/config/connect.js
+++ backend/src/config/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const connect = async () => {
-    return await mongoose.connect("mongodb+srv://ApexSpunk:masai6X@masai.21uwirt.mongodb.net/mock12?retryWrites=true&w=majority");
+    return await mongoose.connect("mongodb+srv://ApexSpunk:masai6X@masai.21uwirt.mongodb.net/cbstocks?retryWrites=true&w=majority");
 };
 
 module.exports = connect;
