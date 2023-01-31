73a8c103f51ed49e6308ea266b5d7b947809cd6d
Chandan <indspunk@gmail.com>
2023-01-31 21:44:08 +0530

    Done

--- backend/src/routes/image.route.js
+++ backend/src/routes/image.route.js
@@ -3,11 +3,11 @@ const app = express.Router();
 const Image = require('../models/image');
 const multer = require('multer');
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
-    },
     filename: function (req, file, cb) {
-        cb(null, file.originalname + '-' + Date.now())
+        cb(null, Date.now() + file.originalname);
+    },
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
     }
 });
 const upload = multer({ storage: storage });
