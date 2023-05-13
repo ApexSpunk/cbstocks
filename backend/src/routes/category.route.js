72430ab2404f93f99e9aeaaf75d262b2c86639c4
Chandan <indspunk@gmail.com>
2023-05-13 11:49:23 +0530

    Done

--- backend/src/routes/category.route.js
+++ backend/src/routes/category.route.js
@@ -17,7 +17,7 @@ app.get('/', async (req, res) => {
     try {
         const categories = await Category.find();
         categories.map(category => {
-            category.image = `https://images.techrapid.in/${category.image}`;
+            category.image = `https://images.techrapid.in/image/${category.image}`;
         });
         res.send({ success: true, categories });
     } catch (error) {
