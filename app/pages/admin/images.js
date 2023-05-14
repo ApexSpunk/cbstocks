f3748e424dcd29e02468458fd5323f959410348d
Chandan <indspunk@gmail.com>
2023-05-14 17:53:17 +0530

    Done

--- app/pages/admin/images.js
+++ app/pages/admin/images.js
@@ -95,9 +95,7 @@ function images({ images, categories, tags, colors }) {
         course.colors.forEach(color => {
             body.append('colors', color);
         });
-        course.keywords.forEach(keyword => {
-            body.append('keywords', keyword);
-        });
+        body.append('keywords', course.keywords);
         body.append('description', course.description);
         const res = await fetch('https://images.techrapid.in/images/upload', {
             method: 'POST',
